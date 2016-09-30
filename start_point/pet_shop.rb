56549e6ced4fcf2c21df2d5abecfa9cce92e3aa8
RLOG89 <ross.loggie@gmail.com>
2016-09-30 15:37:57 +0100

    3 additional tests passed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,3 +1,5 @@
+require("pry-byebug")
+
 def pet_shop_name( shop_name )
   return shop_name[:name]
 end
@@ -17,3 +19,29 @@ end
 def increase_pets_sold( num_pets, addtional_pets_sold )
   return num_pets[:admin][:pets_sold] += addtional_pets_sold
 end
+
+def stock_count( pet_count )
+  return pet_count[:pets].count
+end
+
+
+def pets_by_breed( pet_shop, breed )
+  pets_array = []
+
+  for pet in pet_shop[:pets]
+    if breed == pet[:breed]
+      pets_array.push(pet) 
+    end
+  end
+  return pets_array
+end
+
+# def pets_by_breed( pet_shop, breed )
+#   counter = 0
+#   for pet in pet_shop[:pets]
+#     if breed == pet[:breed]
+#       counter += 1
+#     end
+#   end
+#   return counter
+# end
