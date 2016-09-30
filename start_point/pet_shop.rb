05ced9c9be57a3e94bcf047acf0ae2a9d413e723
RLOG89 <ross.loggie@gmail.com>
2016-09-30 14:07:24 +0100

    3 additional tests passed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -9,3 +9,11 @@ end
 def add_or_remove_cash( pet_shop_cash, cash_added)
   return pet_shop_cash[:admin][:total_cash] += cash_added
 end
+
+def pets_sold( num_pets )
+  return num_pets[:admin][:pets_sold]
+end
+
+def increase_pets_sold( num_pets, addtional_pets_sold )
+  return num_pets[:admin][:pets_sold] += addtional_pets_sold
+end
