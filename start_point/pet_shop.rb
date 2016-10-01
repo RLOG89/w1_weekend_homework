c34690fe7029b43f5c29432d7cea053ba1b39600
David Trail <david@vaunt.eu>
2016-10-01 10:05:49 +0100

    Total cash + add/remove cash

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -2,3 +2,12 @@ def pet_shop_name(obj)
   '''Get pet shop name!'''
   return obj[:name]
 end
+
+def total_cash(obj)
+  return obj[:admin][:total_cash]
+end
+
+def add_or_remove_cash(obj, amount)
+  cash = total_cash(obj)
+  obj[:admin][:total_cash] += amount
+end  
