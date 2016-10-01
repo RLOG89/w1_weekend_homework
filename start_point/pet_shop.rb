dc97ae35befa3ef53a08e397008d4ce2c4222b17
RLOG89 <ross.loggie@gmail.com>
2016-10-01 13:47:42 +0100

    all essential tests passed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -59,9 +59,25 @@ end
 
 def remove_pet_by_name( pet_shop, name )
   for pet in pet_shop[:pets]
-   if pet[:name] == name
-    pet_shop[:pets].delete(pet)
+    if pet[:name] == name
+      pet_shop[:pets].delete(pet)
+    end
   end
 end
+
+def add_pet_to_stock( pet_shop, new_pet )
+  return pet_shop[:pets].push(new_pet)
 end
 
+def customer_pet_count( customer )
+  return customer[:pets].count
+end
+
+def add_pet_to_customer( customer, new_pet )
+  return customer[:pets].push( new_pet ) 
+end
+
+
+def customer_can_afford_pet()
+
+end
