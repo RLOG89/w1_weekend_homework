ce44581c7aedeb69ca9ffd4bb159a3293d42d8f2
David Trail <david@vaunt.eu>
2016-10-01 10:40:53 +0100

    All done

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -32,3 +32,35 @@ def find_pet_by_name(obj, name)
   return nil if a.length == 0
   return a[0]
 end
+
+def remove_pet_by_name(obj, name)
+  obj[:pets].reject! {|e| e[:name] == name}
+end
+
+def add_pet_to_stock(obj, pet)
+  obj[:pets].push(pet)
+end
+
+def customer_pet_count(customer)
+  return customer[:pets].count
+end
+
+def add_pet_to_customer(customer, pet)
+  customer[:pets].push(pet)
+end
+
+## Optional!
+
+def customer_can_afford_pet(customer, pet)
+  customer[:cash] >= pet[:price]
+end
+
+def sell_pet_to_customer(shop, pet, customer)
+  return nil unless pet
+  return nil unless customer_can_afford_pet(customer, pet)
+  add_or_remove_cash(shop, pet[:price])
+  customer[:cash] -= pet[:price]
+  remove_pet_by_name(shop, pet)
+  add_pet_to_customer(customer, pet)
+  increase_pets_sold(shop, 1)
+end
