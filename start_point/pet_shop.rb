cf82728f67026696b293ddad642f1c8724812392
David Trail <david@vaunt.eu>
2016-10-01 10:13:25 +0100

    Breeds!

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -8,6 +8,21 @@ def total_cash(obj)
 end
 
 def add_or_remove_cash(obj, amount)
-  cash = total_cash(obj)
   obj[:admin][:total_cash] += amount
-end  
+end
+
+def pets_sold(obj)
+  return obj[:admin][:pets_sold]
+end
+
+def increase_pets_sold(obj, amount)
+  obj[:admin][:pets_sold] += amount
+end
+
+def stock_count(obj)
+  return obj[:pets].count
+end
+
+def pets_by_breed(obj, breed)
+  return obj[:pets].select{|e| e[:breed] == breed}
+end
